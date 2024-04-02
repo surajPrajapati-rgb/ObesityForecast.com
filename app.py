677bf8571d77236962c40c117c534627ab2512d1
= <jnvsuraj7388@gmail.com>
2024-04-02 07:53:07 +0530

    updated file

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+model = joblib.load('Model/RandomForestClassifier.pkl')
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+
+    data = request.json
+
+    features = [
+        float(data['age']), 
+        float(data['height']), 
+        float(data['weight']),
+        # Convert other features similarly
+    ]
+
+    # features = [1.        , 0.17021277, 0.43754268, 0.12622076, 1.        ,
+    #    1.        , 0.5       , 0.66666667, 0.66666667, 0.        ,
+    #    0.5       , 0.        , 0.        , 1.        , 0.66666667,
+    #    0.        ]
+    
+    features = [features] 
+    
+    prediction = model.predict(features)
+
+    return jsonify({'obesityLevel': prediction.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
