86870dea930533586fb8df146de0d76b8afa98f3
rajat-malvi <malviyaraj2004@gmail.com>
2024-04-04 22:10:26 +0530

    added templates

--- app.py
+++ app.py
@@ -5,27 +5,28 @@ app = Flask(__name__)
 
 model = joblib.load('Model/RandomForestClassifier.pkl')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 
 def predict():
 
     data = request.json
 
-    features = [
-        float(data['age']), 
-        float(data['height']), 
-        float(data['weight']),
-        # Convert other features similarly
-    ]
+    # features = [
+    #     float(data['age']), 
+    #     float(data['height']), 
+    #     float(data['weight']),
+    #     # Convert other features similarly
+    # ]
 
-    # features = [1.        , 0.17021277, 0.43754268, 0.12622076, 1.        ,
-    #    1.        , 0.5       , 0.66666667, 0.66666667, 0.        ,
-    #    0.5       , 0.        , 0.        , 1.        , 0.66666667,
-    #    0.        ]
+    features = [1.        , 0.17021277, 0.43754268, 0.12622076, 1.        ,
+       1.        , 0.5       , 0.66666667, 0.66666667, 0.        ,
+       0.5       , 0.        , 0.        , 1.        , 0.66666667,
+       0.        ]
     
     features = [features] 
     
     prediction = model.predict(features)
+    hello=''
 
     return jsonify({'obesityLevel': prediction.tolist()})
 
